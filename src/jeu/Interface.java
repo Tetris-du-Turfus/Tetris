6aa6fe69da3b64b50c591cdf7255e405943b0584
Clément <Clment@192.168.0.18>
2021-12-06 20:00:22 +0100

    ok

--- src/jeu/Interface.java
+++ src/jeu/Interface.java
@@ -15,7 +15,7 @@ public class Interface extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	int a;
+	int a,b;
 	/**
 	 * Launch the application.
 	 */
