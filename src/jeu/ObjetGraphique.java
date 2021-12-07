f7cc51e8caae7777ccbd79b0796f9616c427c19b
BepoOH <faucheux.clement@gmail.com>
2021-12-07 22:13:08 +0100

    ajout de la classe objetGraphique

---
+++ src/jeu/ObjetGraphique.java
@@ -0,0 +1,14 @@
+package jeu;
+
+public class ObjetGraphique {
+	protected int x;
+	protected int y;
+	protected int tailleCase
+	
+	public ObjetGraphique(int x, int y, int tailleCase) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.tailleCase = tailleCase;
+	};
+}
