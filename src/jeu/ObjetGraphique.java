7ef2ee2f9d2e2885c6c1340e5c8b9910c3602bc6
BepoOH <faucheux.clement@gmail.com>
2021-12-07 22:13:32 +0100

    f

--- src/jeu/ObjetGraphique.java
+++ src/jeu/ObjetGraphique.java
@@ -3,7 +3,7 @@ package jeu;
 public class ObjetGraphique {
 	protected int x;
 	protected int y;
-	protected int tailleCase
+	protected int tailleCase;
 	
 	public ObjetGraphique(int x, int y, int tailleCase) {
 		super();
