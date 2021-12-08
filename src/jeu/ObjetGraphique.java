9def923732f33e6162ef9f9f98a94c0cf4915cfd
BepoOH <faucheux.clement@gmail.com>
2021-12-08 13:58:22 +0100

    set / get

--- src/jeu/ObjetGraphique.java
+++ src/jeu/ObjetGraphique.java
@@ -10,5 +10,29 @@ public class ObjetGraphique {
 		this.x = x;
 		this.y = y;
 		this.tailleCase = tailleCase;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getTailleCase() {
+		return tailleCase;
+	}
+
+	public void setTailleCase(int tailleCase) {
+		this.tailleCase = tailleCase;
 	};
 }
