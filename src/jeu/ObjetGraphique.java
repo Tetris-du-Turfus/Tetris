87b6188f3b283a9da26b01ec454a6762537ccc8c
Camille Euvrard <camille.euvrard38410@gmail.com>
2021-12-16 13:21:50 +0100

    test commit

--- src/jeu/ObjetGraphique.java
+++ src/jeu/ObjetGraphique.java
@@ -56,4 +56,6 @@ public class ObjetGraphique {
 		 // On trace un rectangle dans l'objet de type Graphics passé en paramètre
 		 g.fillRect(this.x,this.y, this.tailleCase,this.tailleCase);
 	 }
+	
+	// test ici
 }
