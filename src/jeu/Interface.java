c461c7a8f0d4e4a36207f21f39d2009ae5cec57f
BepoOH <faucheux.clement@gmail.com>
2021-12-20 19:03:54 +0100

    Ajout d'une petite fonction pour dessiner un carré en haut à gauche

--- src/jeu/Interface.java
+++ src/jeu/Interface.java
@@ -15,15 +15,15 @@ import jeu.MonPanel;
 import javax.swing.JLabel;
 
 import java.awt.Image;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class Interface extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private MonPanel contentPane;
+
+	private JPanel contentPane;
 	int a;
 	/**
 	 * Launch the application.
@@ -44,65 +44,36 @@ public class Interface extends JFrame {
 	
 	public void dessiner(Graphics g)
 	{
-		/*
-		
-		//écriture 5
-		a=5;
-		JLabel lblNewLabel = new JLabel(Integer.toString(a));
-		lblNewLabel.setBounds(185, 118, 45, 13);
-		contentPane.add(lblNewLabel);
-		
-		*/
-			
-		Graphics bufferGraphics;
-		Image offscreen;
-
-		ObjetGraphique o;
-		
-		//objet graphique coordonnées : 10,10 et taille de la case : 5
-		o = new ObjetGraphique(10,10,5);
-		
-		System.out.println(this.getContentPane().getWidth()); // retourne 0, je comprends pas pourquoi
-		
-		// On crée une image en mémoire de la taille du ContentPane
-		//Le problème est ici : createImage retourne null, celà signifie que l'image n'est pas affichable
-		offscreen = createImage(this.getContentPane().getWidth(),this.getContentPane().getHeight());
-		
-		// On récupère l'objet de type Graphics permettant de dessiner dans cette image
-		bufferGraphics = offscreen.getGraphics();
-		// On colore le fond de l'image en blanc
-		bufferGraphics.setColor(Color.WHITE);
-		bufferGraphics.fillRect(0,0,this.getContentPane().getWidth(),this.getContentPane().getHeight());
-		
-		// On dessine les objets graphiques de la liste dans l'image en mémoire pour éviter les
-		// problèmes de scintillements
-
-		o.Afficher(bufferGraphics);
-		// On afficher l'image mémoire à l'écran
-		g.drawImage(offscreen,0,getContentPane().getWidth(),null);
-		
-		
-		
-		
-	} 
-
+	 Graphics bufferGraphics;
+	 Image offscreen;
+	 // On crée une image en mémoire de la taille du ContentPane
+	 offscreen = createImage(this.getContentPane().getWidth(),this.getContentPane().getHeight());
+	 // On récupère l'objet de type Graphics permettant de dessiner dans cette image
+	 bufferGraphics = offscreen.getGraphics();
+	 // On colore le fond de l'image en blanc
+	 bufferGraphics.setColor(Color.WHITE);
+	 bufferGraphics.fillRect(0,0,this.getContentPane().getWidth(),this.getContentPane().getHeight());
+	 // On dessine les objets graphiques de la liste dans l'image en mémoire pour éviter les
+	 // problèmes de scintillements
+	 bufferGraphics.setColor(Color.RED);
+	 bufferGraphics.fillRect(0,0,50,50);
+	 // On afficher l'image mémoire à l'écran
+	 g.drawImage(offscreen,0,0,null);
+	}
+	
 	/**
 	 * Create the frame.
 	 */
 	public Interface() {
 		
-		contentPane = new MonPanel(this);
-
-		
-		contentPane.setFocusable(true);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		this.setContentPane(contentPane);
-		contentPane.setLayout(null);
-
 		this.getContentPane().setBackground(Color.WHITE);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
 		
 		//écriture 5
@@ -111,8 +82,16 @@ public class Interface extends JFrame {
 		lblNewLabel.setBounds(185, 118, 45, 13);
 		contentPane.add(lblNewLabel);
 		
+		JButton btnNewButton = new JButton("Jouer");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dessiner(contentPane.getGraphics());
+			}
+		});
+		btnNewButton.setBounds(104, 153, 85, 21);
+		contentPane.add(btnNewButton);
+		
 		//appelle dessiner pour dessiner une case
-		dessiner(this.getContentPane().getGraphics());
 		
 		//this.fillRect (10, 10, 200, 200);  
 		
